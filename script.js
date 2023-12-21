86bb15ffd46c1f824b8efbe9c3a17592f537570f
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-21 15:49:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const makeChange = (c) => {
   // your name here
+	let ans={
+		q:0,
+		d:0,
+		n:0,
+		p:0
+	};
+	
+	while (c) {
+		if(c>=25){
+			ans["q"]++;
+			c-=25;
+		}
+		else if(c>=10){
+			ans["d"]++;
+			c-=10;
+		}
+		else if(c>=5){
+			ans["n"]++;
+			c-=5;
+		}
+		else {
+			ans["p"]++;
+			c-=1;
+		}
+	}
+	return ans;
 };
 
 // Do not the change the code below
